b0fde76d6fe8e4e72c12949b269d7d77eb8e399b
zjq <-->
2025-07-12 16:46:01 +0800

    新增主页猜你喜欢、今日推荐、热卖排行模块

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -2,6 +2,9 @@
     <div>
         <TypeNav></TypeNav>
         <ListContainer></ListContainer>
+        <TodayRecommend></TodayRecommend>
+        <Rank></Rank>
+        <Like></Like>
     </div>
 </template>
 
@@ -9,10 +12,16 @@
 <script>
 import TypeNav from './TypeNav';
 import ListContainer from './ListContainer';
+import TodayRecommend from './TodayRecommend'
+import Rank from './Rank'
+import Like from './Like'
 export default{
     components:{
         TypeNav,
         ListContainer,
+        TodayRecommend,
+        Rank,
+        Like,
     }
 }
 </script>
